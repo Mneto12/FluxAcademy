63e50dd1af31fdee45062126d0c31ffae48b64ae
Miguel <envenezuela2000@gmail.com>
2022-07-25 02:54:09 -0400

    update dashboard

--- vista/index.php
+++ vista/index.php
@@ -123,7 +123,7 @@ if(isset($_POST['btncerrar']))
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="/" class="brand-link">
+      <a href="../vista/index.php" class="brand-link">
         <img src="../assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Flux Academy</span>
       </a>
